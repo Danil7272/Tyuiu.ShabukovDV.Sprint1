27f77ec219abc805a38e728ced7851b43ba01f6c
Данил Шабуков <danilshabukov8@gmail.com>
2023-10-14 20:42:55 +0400

    Task2

---
+++ Tyuiu.ShabukovDV.Sprint1.Task2.V0.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ShabukovDV.Sprint1.Task2.V0.Lib
+{
+    public class DataService : ISprint1Task2V30
+    {
+        public double ConvertKmToMetre(double value)
+        {
+            return value * value;
+        }
+    }
+}
---
+++ Tyuiu.ShabukovDV.Sprint1.Task2.V0/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShabukovDV.Sprint1.Task2.V0.Lib;
+
+
+namespace Tyuiu.ShabukovDV.Sprint1.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+
+            Console.WriteLine("Введите значения X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Число в километрах X = " + ds.ConvertKmToMetre(x));
+
+            Console.ReadLine();
+        }
+    }
+}
