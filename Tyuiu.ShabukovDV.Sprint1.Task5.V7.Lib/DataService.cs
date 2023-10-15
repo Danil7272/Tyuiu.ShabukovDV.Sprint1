1956246bb1adb1fbaa92ef4bbd58909224829f8a
Данил Шабуков <danilshabukov8@gmail.com>
2023-10-15 13:32:44 +0400

    Task5

---
+++ Tyuiu.ShabukovDV.Sprint1.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using OpenXmlPowerTools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ShabukovDV.Sprint1.Task5.V7.Lib
+{
+    public class DataService : ISprint1Task5V7
+    {
+        public int AngleToHoursMinutes(double f)
+        {
+            double h;
+            h = f / 30;
+            return (int)h;
+        }
+    }
+}
---
+++ Tyuiu.ShabukovDV.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ShabukovDV.Sprint1.Task5.V7.Lib;
+
+namespace Tyuiu.ShabukovDV.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int res = ds.AngleToHoursMinutes(100);
+
+            Assert.AreEqual(3, res);
+        }
+    }
+}
