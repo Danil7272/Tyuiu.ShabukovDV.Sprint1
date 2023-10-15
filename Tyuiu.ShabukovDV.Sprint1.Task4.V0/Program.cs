168c7689e1350a9bddb6914ffe1b728e048bae61
Данил Шабуков <danilshabukov8@gmail.com>
2023-10-15 13:13:59 +0400

    Task4

---
+++ Tyuiu.ShabukovDV.Sprint1.Task4.V0/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShabukovDV.Sprint1.Task4.V0.Lib;
+
+namespace Tyuiu.ShabukovDV.Sprint1.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y;
+
+            Console.WriteLine("Введите значение x");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y");
+            y = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("1 / (x ^ 2 + y ^ 2) = " + ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
